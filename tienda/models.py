dc7ae4d81639e1549ab018cd108ead6b019ee0b0
DarkUsagi13 <hector.martinez.cornejo.al@iespoligonosur.org>
2022-10-31 10:24:55 +0100

    Modelos creados

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=255, primary_key=True)
+
+
+class Product(models.Model):
+    modelo = models.ForeignKey('Marca', on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=200)
+    unidades = models.IntegerField(default=1)
+    precio = models.FloatField(default=0)
+    detalles = models.TextField(blank=True, max_length=255)
